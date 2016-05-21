5f3397c604f34833f2ccbd0c630bf80d47a9356c
Spencer Watson <spencer@wattydev.com>
2016-05-21 10:23:08 -0700

    test

--- controllers/index.php
+++ controllers/index.php
@@ -5,13 +5,12 @@ $data = (object) array(
     'title' => false
 );
 var_dump($data);
-init();
+$data = array_merge((array) $data, (array) getData());
 
-function init() {
+function getData() {
     $posts = ds_getPosts();
 
-    var_dump($data);
-    var_dump($posts);
+    $data = (object) array();
     // Set recent portfolio items
     $data->portfolio = array_filter($posts->posts, function ($post) {
         return $post->type !== "46" && $post->type !== 46;
@@ -21,4 +20,8 @@ function init() {
     $data->$blog = array_filter($posts->posts, function ($post) {
         return $post->type === "46" || $post->type === 46;
     });
+
+    return $data;
 }
+
+var_dump($data);
