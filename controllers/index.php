93eaf00de1e16c1cf6834db0128eed6a35464d9e
Spencer Watson <spencer@wattydev.com>
2016-05-21 10:18:13 -0700

    test

--- controllers/index.php
+++ controllers/index.php
@@ -7,6 +7,8 @@ init();
 function init() {
     $posts = ds_getPosts();
 
+    var_dump($data);
+    var_dump($posts);
     // Set recent portfolio items
     $data->portfolio = array_filter($posts->posts, function ($post) {
         return $post->type !== "46" && $post->type !== 46;
