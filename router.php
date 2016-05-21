06928688d20bb8e9c9979003213c1be38859cc2a
Spencer Watson <spencer@wattydev.com>
2016-05-20 21:14:03 -0700

    test

--- router.php
+++ router.php
@@ -6,7 +6,10 @@ $uriMap = (object) array(
     "type" => "type.php",
     "*" => "post.php"
 );
-$directory = explode('/', $uri);
+$directory = explode('/', $uri)[1];
+if ($directory === "") {
+    $directory = "/";
+}
 var_dump($uri);
 var_dump($directory);
 echo 'You got routed, son!';
