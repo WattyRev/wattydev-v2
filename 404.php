e0aaf2fc2e2f3cf0fd7ceadf51c35e684a4c4c82
Spencer Watson <spencer@wattydev.com>
2016-05-20 21:11:31 -0700

    test

---
+++ 404.php
@@ -0,0 +1,2 @@
+<?php
+echo 'I donno watchu be lookin fo, but it aint ere.';
--- router.php
+++ router.php
@@ -1,4 +1,12 @@
 <?php
 $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+$uriMap = (object) array(
+    "/" => "index.php",
+    "tag" => "tag.php",
+    "type" => "type.php",
+    "*" => "post.php"
+);
+$directory = $uri.explode('/');
 var_dump($uri);
+var_dump($directory);
 echo 'You got routed, son!';
