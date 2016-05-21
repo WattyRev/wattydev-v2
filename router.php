93f59d2d2890286e2f8d8499debc75eaa67258af
Spencer Watson <spencer@wattydev.com>
2016-05-20 22:14:12 -0700

    test

--- router.php
+++ router.php
@@ -3,30 +3,32 @@
  * Route the user the the relevant location
  */
 
-$router = (object) array();
+class Router {
+    public function getTemplate() {
+        $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
 
-$router->getTemplate = function() {
-    $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
+        // A map of directories and what php file to use
+        $uriMap = array(
+            "/" => "index.php",
+            "t" => "tag.php",
+            "c" => "type.php",
+            "*" => "post.php"
+        );
 
-    // A map of directories and what php file to use
-    $uriMap = array(
-        "/" => "index.php",
-        "t" => "tag.php",
-        "c" => "type.php",
-        "*" => "post.php"
-    );
-
-    // Include the correct file
-    $directory = explode('/', $uri)[1];
-    if ($directory === "") {
-        $directory = "/";
+        // Include the correct file
+        $directory = explode('/', $uri)[1];
+        if ($directory === "") {
+            $directory = "/";
+        }
+        if (isset($uriMap[$directory])) {
+            return $uriMap[$directory];
+        } else {
+            return $uriMap["*"];
+        }
     }
-    if (isset($uriMap[$directory])) {
-        return $uriMap[$directory];
-    } else {
-        return $uriMap["*"];
-    }
-};
+}
+
+$router = new Router();
 
 $template = $router->getTemplate();
 
