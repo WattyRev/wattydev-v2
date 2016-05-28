7ebc8d0964fbd489eaf325f7d0a31bb95ccd33f7
Spencer Watson <spencer@wattydev.com>
2016-05-27 21:52:04 -0700

    set up email script

--- contact.php
+++ contact.php
@@ -1,4 +1,13 @@
 <?php
 include 'api/init.php';
 
-var_dump($_POST);
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'] . "\n\nSent from wattydev.com";
+$message = wordwrap($message,70);
+$headers = "From: noreply@wattydev.com" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+// send email
+mail("spencer@wattydev.com",$subject,$message,$headers);
