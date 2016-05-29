78db2d3764ca48e1961d66a6a14a77c099eee2f0
Spencer Watson <spencer@wattydev.com>
2016-05-28 18:34:17 -0700

    add google analytics

--- templates/layout.php
+++ templates/layout.php
@@ -69,5 +69,16 @@ I have been requested by the Nigerian National Petroleum Company to contact you 
         <?php foreach(get_js() as $file):?>
             <script src="<?php echo build_url('js', $file); ?>"></script>
         <?php endforeach; ?>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-35884972-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
